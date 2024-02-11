a282e24029d316497b44cca02ce85ccad7fba708
kcg07 <kylecgo07@gmail.com>
2024-02-12 00:28:38 +0100

    Pujada UF1_Projecte1

--- UF1_PelisProyecto0/app/src/main/java/com/exercicisgokyle/uf1_pelisproyecto0/network/response/PeliculaResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.exercicisgokyle.uf1_pelisproyecto0.network.response
-
-import com.exercicisgokyle.uf1_pelisproyecto0.models.PeliculaModel
-import com.google.gson.annotations.SerializedName
-
-data class PeliculaResponse(
-    @SerializedName("results")
-    var resultados: List<PeliculaModel>
-)
